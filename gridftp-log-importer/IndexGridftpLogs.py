dd877d75fc47749ed47cb8c6f320bd5719e6909c
Deepak Nadig <admin@deepaknadig.com>
2017-11-20 17:23:24 -0600

    Updated GridFTP log Indexer for @timestamp.

--- gridftp-log-importer/IndexGridftpLogs.py
+++ gridftp-log-importer/IndexGridftpLogs.py
@@ -6,21 +6,28 @@ from elasticsearch import helpers
 from dateutil import parser
 
 # Set the directory in which the log files are stored
-dir_list = os.fsencode("/home/dna/gftplogs")
+dir_list = os.fsencode("/home/ubuntu/gftplogs")
 
 
 def readEntry():
     for filename in os.listdir(dir_list):
         # Create the complete file path.
         file = open(os.path.join(dir_list, filename))
-        # print("Indexing File: " + str(filename, 'utf-8'))
+        print("Indexing File: " + str(filename, 'utf-8'))
         try:
             reader = csv.reader(file, delimiter=" ")
             for line in reader:
                 # Create a Single Entry.
                 timestamp = parser.parse(line[0])
-                entry = {'@timestamp': timestamp, 'source': line[1], 'event': line[2], 'src_ip': line[3],
-                         'src_port': int(line[4]), 'username': line[5], 'filename': line[6], 'direction': line[7]}
+                entry = {}
+                entry['@timestamp'] = timestamp
+                entry['source'] = line[1]
+                entry['event'] = line[2]
+                entry['src_ip'] = line[3]
+                entry['src_port'] = int(line[4])
+                entry['username'] = line[5]
+                entry['filename'] = line[6]
+                entry['direction'] = line[7]
                 yield entry
                 # print(entry)
         except:
